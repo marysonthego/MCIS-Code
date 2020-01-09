d549c4abfd32a3ad146f26000f2f9d4e7e2b5c5e
marysonthego <44341320+marysonthego@users.noreply.github.com>
2020-01-09 15:07:47 -0700

    commit 8

--- web-projects/express-project/routes/index.js
+++ web-projects/express-project/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var jsonData = require('../data.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Express',
+    data: jsonData });
 });
 
 module.exports = router;
